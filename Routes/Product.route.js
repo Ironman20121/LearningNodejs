0720a1b365cadd4026b8b1ec45bd95d5b0bf5891
phycsepy <kundancseb2@vjit.ac.in>
2021-05-23 19:38:02 +0530

    final commit-1

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -3,77 +3,24 @@ const express = require('express')
 
 const router = express.Router();
 
-const Product = require('../Models/Product.model')
+const ProductController = require('../Controllers/Product.controller')
+
 //get all products
-router.get('/',async(req,res,next)=>{
- try {
-     const results = await Product.find({},{__v:0})
-     res.send(results)
- } catch (error) {
-     console.log(result)
- }
-    
+router.get('/',ProductController.getAllProducts)
 
-})
-//creating new product
-router.post('/',async(req,res,next)=>{
-  
 
-    // using promises 
-    // product.save()
-    //         .then(result=>{
-    //             console.log(result)
-    //             res.send(result)
-    //         })
-    //         .catch(err =>{
-    //             console.log(err)
-    //             res.send(err.message)
-    //         })
-     
-    try {
-        const product = new Product(req.body)
-        const result = await product.save()
-        res.send(result)
-        
-    } catch (error) {
-        console.log(error.message)
-        
-    }
-})
+//get products by id 
+router.get('/:id',ProductController.getProductById)
+//delete element by id 
 
-router.get('/:id',async(req,res,next)=>{
-    const id = req.params.id;
-    try {
-    const product= await Product.findById(id)
-        res.send(product)
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+router.delete('/:id',ProductController.deleteElementById)
 
-router.patch('/:id',async(req,res,next)=>{   
-    const id  = req.params.id
-    const updates = req.body
-    const options = {new:true}
-    try {
-        
-        const result = await Product.findByIdAndUpdate(id,updates,options)
-        res.send(result)
-    } catch (error) {
-        console.error(error.message)
-    }
-    
-})
+//Updating ele by id 
+router.patch('/:id',ProductController.updatingElementbyId)
 
-router.delete('/:id',async(req,res,next)=>{
-    const id = req.params.id
-    try {
-        const result = await Product.findByIdAndDelete(id)
-        res.send(result)
-        console.log(result)
-    } catch (error) {
-        console.log(error.message)
-        
-    }
-})
+//Create a new product
+router.post('/',ProductController.creatingNewProduct)
+
+
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const express = require('express')
-
-const mongoose = require('mongoose')
-
-const createError = require('http-errors')
-
+const express = require('express');
+const createError = require('http-errors');
+const dotenv = require('dotenv').config()
 const app = express()
 
 app.use(express.json())
@@ -11,58 +8,32 @@ app.use(express.urlencoded({ extended: true }))
 
 
 const ProductRoutes = require('./Routes/Product.route')
-const { create } = require('./Models/Product.model')
+
 app.use('/products',ProductRoutes)
 
-mongoose.connect('mongodb+srv://cluster0.bwgam.mongodb.net/',{
-    user:'kundan',
-    pass:'murali1234',
-    dbName:'learningdb',
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useFindAndModify:false 
-}).then(()=>{
-    console.log('mongodb conneted ....')
-}).catch((err)=>{
-    console.log(err)
-})
+//conneting mongodb
+require('./initDB')()
 
-
-//basic middle ware
-// app.use((req,res,next)=>{
-//     res.status(404)
-//     res.send({
-//         error:'Not found'
-//     })
-// })
-
-// app.all('/test',(req,res)=>{
-    // console.log(req.query)
-    // res.send(req.query)
-    // console.log(req.params)
-    // res.send(req.params)
-//     console.log(req.body)
-//     res.send(req.body)
-// })
-
-//middle ware erroe handler
-app.use((req,res,next)=>{
-    next(createError(404,'Not found'))
-  })
-
-
-app.use((err,req,res,next)=>{
-    //this can called from any whre eg routes next()
-    res.status(err.status||500)
+//404 handler and pass to error handler
+app.use((req, res, next) => {
+    
+    next(createError(404, 'Not found'));
+  });
+  
+  //Error handler
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
     res.send({
-        error:{
-            status:err.status||500,
-            message:err.message,
-        }
-    })
+      error: {
+        status: err.status || 500,
+        message: err.message
+      }
+    });
+  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`Server start on port ${PORT}....`)
 })
 
 
-app.listen(3000,()=>{
-    console.log('sever started on port 3000.....')
-})
+
